5db81be7190c6c1ab3d8669f5447bb780c9273be
Jackie Cheung <jackcheung56@gmail.com>
2020-12-03 14:59:56 -0500

    start account router and controller

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,6 +1,8 @@
 const Router = require('express').Router()
 const UserRouter = require ('./UserRouter')
+const AccountRouter = reuqire ('./AccountRouter')
 
 Router.use('/user', UserRouter)
+Router.use('/account', AccountRouter)
 
 module.exports = Router
